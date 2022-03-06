c266310722bfe916f37dd35ee3889318ae5a6ebf
Dipankar <dipankar15-11137@diu.edu.bd>
2022-03-06 20:04:11 +0600

    add practice

---
+++ prctic/app.js
@@ -0,0 +1,18 @@
+const inputValue = () => {
+
+    const number = parseFloat(prompt('enter a number'));
+    let sum = number + 200;
+    alert(sum);
+    console.log(sum)
+}
+const inputValue2 = () => {
+
+    const value = confirm('Picnic a jabe bal');
+    if (value) {
+        console.log('https://web.programming-hero.com/web-5/post/web-5-text-instruction-important-module-40-5-browser-api-practice');
+    }
+    else {
+        alert('Dura gia mor');
+    }
+
+}
